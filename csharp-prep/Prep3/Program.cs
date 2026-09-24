f42afb633fbdee7cef0c58443dd945d250229302
Newel Wise <newel.h.wise@gmail.com>
2026-09-24 15:54:55 -0600

    Write instead of writeline.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101);
-        Console.WriteLine("What is your guess? ");
+        Console.Write("What is your guess? ");
         int guess = int.Parse(Console.ReadLine());
         while (guess != magicNumber)
         {
@@ -18,7 +18,7 @@ class Program
             {
                 Console.WriteLine("Higher");
             }
-            Console.WriteLine("What is your guess? ");
+            Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
         } 
         Console.WriteLine("You guessed it! ");
