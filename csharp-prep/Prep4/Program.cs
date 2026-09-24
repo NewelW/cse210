895bb3fee1d07f3f534727a48b5f585f4567cd07
Newel Wise <newel.h.wise@gmail.com>
2026-09-24 16:13:34 -0600

    Complete with another, doubles are better for that math to retain better accuracy if needed.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int number = -1;
+        List<int> numbers = new List<int>();
+        while (number != 0)
+        {
+            Console.Write("Enter Number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        int sum = 0;
+        int largestNumber = 0;
+        foreach (int num in numbers)
+        {
+            sum = sum + num;
+            if (num > largestNumber)
+            {
+                largestNumber = num;
+            }
+        }
+        double average = (double)sum/numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
