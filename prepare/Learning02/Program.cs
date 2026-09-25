4ad75c7d89e222911e8be09e9a11d3803e0f414b
Newel Wise <newel.h.wise@gmail.com>
2026-09-24 18:16:11 -0600

    Learning02 Done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microslop";
+        job1._endYear = 2028;
+        job1._startYear = 2026;
+        Job job2 = new Job();
+        job2._jobTitle = "Electrical Engineer";
+        job2._company = "Apple";
+        job2._endYear = 2047;
+        job2._startYear = 2002;
+        Resume resume = new Resume();
+        resume._name = "Dave Joe";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.DisplayResume();
     }
 }
